3339da9c8223d11935014d673d54c1ce5f6873ff
leftarion1 <Andrey462kk@gmail.com>
2024-11-02 16:18:14 +0300

    Добавил класс игрока и функцию изменения скорости. Изменил проверку и передачу данных в клиентскую часть. Доработал игровой цикл, установил фпс. Создал серверное окно.

--- server.py
+++ server.py
@@ -1,36 +1,126 @@
 import socket
 import time
+import pygame
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("postgresql://postgres:test_postgres@localhost:5433/bacterias")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+s = Session()
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 main_socket.bind(("localhost", 10000))
 main_socket.setblocking(False)
 main_socket.listen(5)
 print("Сокет создался")
-players = []
-while True:
+
+#Создание серверного окна
+pygame.init()
+WIDHT_ROOM, HEIGHT_ROOM = 4000, 4000
+WIDHT_SERVER, HEIGHT_SERVER = 300, 300
+FPS = 100
+screen = pygame.display.set_mode((WIDHT_SERVER, HEIGHT_SERVER))
+pygame.display.set_caption("Сервер")
+clock = pygame.time.Clock()
+
+
+class Player(Base):
+    __tablename__ = "gamers"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250))
+    address = Column(String)
+    x = Column(Integer, default=500)
+    y = Column(Integer, default=500)
+    size = Column(Integer, default=50)
+    errors = Column(Integer, default=0)
+    abs_speed = Column(Integer, default=2)
+    speed_x = Column(Integer, default=2)
+    speed_y = Column(Integer, default=2)
+
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+class LocalPlayer:
+    def __init__(self, id, name, sock, addr):
+        self.id = id
+        self.db: Player = s.get(Player, self.id)
+        self.sock = sock
+        self.name = name
+        self.address = addr
+        self.x = 500
+        self.y = 500
+        self.size = 50
+        self.errors = 0
+        self.abs_speed = 1
+        self.speed_x = 0
+        self.speed_y = 0
+
+    def update(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+
+Base.metadata.create_all(bind=engine)
+
+players = {}
+server_works = True
+while server_works:
+    clock.tick(FPS)
 
     try:
         # проверяем желающих войти в игру
         new_socket, addr = main_socket.accept()  # принимаем входящие
         print('Подключился', addr)
         new_socket.setblocking(False)
-        players.append(new_socket)
+        player = Player("Имя", addr)
+        s.merge(player)
+        s.commit()
+        addr = f'({addr[0]},{addr[1]})'
+        data = s.query(Player).filter(Player.address == addr)
+        for user in data:
+            player = LocalPlayer(user.id, "Имя", new_socket, addr)
+            players[user.id] = player
 
     except BlockingIOError:
         pass
-    for sock in players:
+    # Считываем команды игроков
+    for id in list(players):
         try:
-            data = sock.recv(1024).decode()
+            data = players[id].sock.recv(1024).decode()
             print("Получил", data)
         except:
             pass
-    # Отправка игрокам поля
-    for sock in players:
+    # Отправляем статус игрового поля
+    for id in list(players):
         try:
-            sock.send("LOL".encode())
+            players[id].sock.send("Игра".encode())
         except:
-            players.remove(sock)
-            sock.close()
+            players[id].sock.close()
+            del players[id]
+            # Так же удаляем строчку из БД
+            s.query(Player).filter(Player.id == id).delete()
+            s.commit()
             print("Сокет закрыт")
-    time.sleep(1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            server_works = False
+    screen.fill('black')
+    for id in players:
+        player = players[id]
+        x = player.x * WIDHT_SERVER // WIDHT_ROOM
+        y = player.y * HEIGHT_SERVER // HEIGHT_ROOM
+        size = player.size * WIDHT_SERVER // WIDHT_ROOM
+        pygame.draw.circle(screen, "yellow2", (x, y), size)
+        pygame.display.update()
+pygame.quit()
+main_socket.close()
+s.query(Player).delete()
+s.commit()
+
+
